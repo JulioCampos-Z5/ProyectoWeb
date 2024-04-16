69f60e48252f58ae64279668cd1ac06bd907edd9
Julio Campos <julioz5435@gmail.com>
2024-04-16 10:01:25 -0600

    implementacion de php

---
+++ PROYECTO_SSPBD/PHP/Vender.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>no entrar</title>
+</head>
+
+<body>
+    <h1>Hoa</h1>
+</body>
+
+</html>
