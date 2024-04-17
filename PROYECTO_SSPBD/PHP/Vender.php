3e37f7b4488980f10b81330990651f49db7b9a94
Julio Campos <julioz5435@gmail.com>
2024-04-17 00:31:56 -0600

    M

--- PROYECTO_SSPBD/PHP/Vender.php
+++ PROYECTO_SSPBD/PHP/Vender.php
@@ -18,13 +18,18 @@
 
         table {
             width: 100%;
-            background-color: black;
+            border-collapse: collapse;
+            /* Combina los bordes de las celdas adyacentes */
         }
 
         tr {
-            height: 30PX;
-            background-color: #DADFE1;
+            height: 30px;
             color: black;
+        }
+
+        td {
+            border: 1px solid black;
+            /* Cambié 10000PX a 2px solid black */
         }
     </style>
 </head>
@@ -38,21 +43,26 @@
             <td>Talla</td>
             <td>Precio</td>
         </tr>
-        <? ?>
-        <tr>
-            <td>1</td>
-            <td>xxx</td>
-            <td>Playera</td>
-            <td>M</td>
-            <td>499</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>xxx</td>
-            <td>Playera</td>
-            <td>M</td>
-            <td>499</td>
-        </tr>
+        <?php
+        $numero_de_tablas = 3; // Define el número de tablas que deseas generar
+
+        // Bucle para generar múltiples tablas
+        for ($i = 0; $i < $numero_de_tablas; $i++) { ?>
+            <tr>
+                <td>1</td>
+                <td>xxx</td>
+                <td>Playera</td>
+                <td>M</td>
+                <td>499</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>xxx</td>
+                <td>Playera</td>
+                <td>M</td>
+                <td>499</td>
+            </tr>
+        <?php } ?>
     </table>
 </body>
 
