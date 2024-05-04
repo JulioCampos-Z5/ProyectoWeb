0c57c2281c743cefb5ce286d27dfa727947a555c
Julio Campos <julioz5435@gmail.com>
2024-05-04 00:31:07 -0600

    N

--- PROYECTO_SSPBD/PHP/Vender.php
+++ PROYECTO_SSPBD/PHP/Vender.php
@@ -1,13 +1,10 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>no entrar</title>
+    <title>No entrar</title>
     <style>
         body {
             font-family: Arial, Helvetica, sans-serif;
@@ -15,35 +12,38 @@
             border: 0;
             padding: 0;
             background-color: #C0C9CD;
-            float: left;
+        }
+
+        nav {
+            margin: 15px;
+            border-top-left-radius: 15px;
+            border-bottom-left-radius: 15px;
         }
 
         nav ul {
             list-style-type: none;
             margin: 0;
             padding: 0;
+            display: flex;
         }
 
         nav ul li {
-            float: left;
+            margin: 0;
+            padding: 0;
         }
 
         nav ul li a {
             display: block;
-            color: rgb(0, 0, 0);
+            color: black;
             text-align: center;
-            padding: 20px 70px;
+            padding: 14px 16px;
             text-decoration: none;
         }
 
         nav ul li a:hover {
-            background-color: #ffffff;
+            background-color: #ddd;
             color: black;
             border-radius: 50px;
-        }
-
-        li {
-            margin: 5px;
         }
 
         a {
@@ -52,77 +52,73 @@
         }
 
         main {
-            display: grid;
-            grid-template-columns: 427px 1px;
+            display: flex;
         }
 
         .Venta {
-            margin: 5px;
-            border-radius: 40px;
+            margin: 0px 10px 0px 20px;
+            border-radius: 20px;
             background-color: #DADFE1;
-            width: 414px;
-            height: 480px;
+            width: calc(50% - 10px);
+            max-width: 300px;
+            padding: 20px;
+            box-sizing: border-box;
         }
 
         .Venta h1 {
-            padding: 13px 0px 0px 0px;
-            font-size: 28px;
+            font-size: 24px;
             text-align: center;
+            margin-top: 0;
         }
 
         table {
-            margin: 10px 0px 0px 6px;
-            background-color: #DADFE1;
-            border-radius: 50px;
-            height: 480px;
-            width: 1000px;
+            margin: 0px 20px 0px 0px;
+            width: calc(100% - 10px);
 
         }
 
-        #Barra {
-            margin: 10px;
-            border-radius: 15px;
-            font-size: 30px;
-            height: 54px;
-            width: 92%;
+        th,
+        td {
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        td {
+            background-color: #fff;
         }
 
         .Boton {
-            margin: 10px;
+            margin: 10px 5px;
             background-color: #C0C9CD;
-            /* Cambiado el color de fondo */
             border-radius: 50px;
             font-size: 15px;
             height: 54px;
-            width: 177px;
+            width: calc(50% - 10px);
             border: none;
-            /* Eliminar el borde */
             cursor: pointer;
-            /* Cambiar el cursor al pasar sobre los botones */
             transition: background-color 0.3s ease;
-            /* Transición suave para el cambio de color de fondo */
         }
 
         .Boton:hover {
             background-color: #ffffff;
-            /* Cambiar el color de fondo al pasar el cursor */
         }
 
+        @media screen and (max-width: 600px) {
+            nav ul li {
+                float: none;
+                width: auto;
+                text-align: center;
+            }
 
-        table {
-            width: 1000px;
-            border-collapse: collapse;
-            /* Combina los bordes de las celdas adyacentes */
-        }
-
-        tr {
-            height: 30px;
-            color: black;
-        }
-
-        td {
-            border: 1px solid black;
-            /* Cambié 10000PX a 2px solid black */
+            .Venta,
+            table {
+                width: 100%;
+                max-width: none;
+            }
         }
     </style>
 </head>
@@ -134,50 +130,45 @@
             <li><a href="#">Devolución</a></li>
         </ul>
     </nav>
-    <br>
-    <br>
-    <Br>
-    <Br>
     <main>
         <div class="Venta">
-            <h1>ingrece código del producto</h1>
+            <h1>Ingrese código del producto</h1>
             <input type="text" name="Barra" id="Barra"> <br>
             <input class="Boton" type="button" value="Agregar">
             <input class="Boton" type="button" value="Escanear producto"> <br>
             <input class="Boton" type="button" value="Eliminar">
             <input class="Boton" type="button" value="Cobrar">
         </div>
-        <main>
-            <table>
-                <tr>
-                    <td>N</td>
-                    <td>Producto</td>
-                    <td>Categoria</td>
-                    <td>Talla</td>
-                    <td>Precio</td>
-                </tr>
-                <?php
-                $numero_de_tablas = 3; // Define el número de tablas que deseas generar
+        <table>
+            <tr>
+                <th>N</th>
+                <th>Producto</th>
+                <th>Categoría</th>
+                <th>Talla</th>
+                <th>Precio</th>
+            </tr>
+            <?php
+            $numero_de_tablas = 3; // Define el número de tablas que deseas generar
 
-                // Bucle para generar múltiples tablas
-                for ($i = 0; $i < $numero_de_tablas; $i++) { ?>
-                    <tr>
-                        <td>1</td>
-                        <td>xxx</td>
-                        <td>Playera</td>
-                        <td>M</td>
-                        <td>499</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>xxx</td>
-                        <td>Playera</td>
-                        <td>M</td>
-                        <td>499</td>
-                    </tr>
-                <?php } ?>
-            </table>
-        </main>
+            // Bucle para generar múltiples tablas
+            for ($i = 0; $i < $numero_de_tablas; $i++) { ?>
+                <tr>
+                    <td>1</td>
+                    <td>xxx</td>
+                    <td>Playera</td>
+                    <td>M</td>
+                    <td>499</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>xxx</td>
+                    <td>Playera</td>
+                    <td>M</td>
+                    <td>499</td>
+                </tr>
+            <?php } ?>
+        </table>
+    </main>
 </body>
 
 </html>
